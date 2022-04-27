a54424123efe4e23cd035b23400ab38321013514
Ilia Platone <info@iliaplatone.com>
2022-04-27 14:02:11 +0200

    Initial release

---
+++ ahp_hub.c
@@ -0,0 +1,90 @@
+﻿/**
+*    MIT License
+*
+*    libahp_hub library to drive the AHP HUB controllers
+*    Copyright (C) 2021  Ilia Platone
+*
+*    Permission is hereby granted, free of charge, to any person obtaining a copy
+*    of this software and associated documentation files (the "Software"), to deal
+*    in the Software without restriction, including without limitation the rights
+*    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*    copies of the Software, and to permit persons to whom the Software is
+*    furnished to do so, subject to the following conditions:
+*
+*    The above copyright notice and this permission notice shall be included in all
+*    copies or substantial portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*    SOFTWARE.
+*/
+
+#include "ahp_hub.h"
+#include "rs232.c"
+#include <pthread.h>
+
+static int mutexes_initialized = 0;
+static pthread_mutexattr_t mutex_attr;
+static pthread_mutex_t mutex;
+static unsigned int ahp_hub_connected = 0;
+
+
+int ahp_hub_connect_fd(int fd)
+{
+    if(ahp_hub_is_connected())
+        return 0;
+    if(fd != -1) {
+        ahp_serial_SetFD(fd, 9600);
+        ahp_hub_connected = 1;
+    }
+    return 1;
+}
+
+int ahp_hub_get_fd()
+{
+    if(!ahp_hub_is_connected())
+        return -1;
+    return ahp_serial_GetFD();
+}
+
+int ahp_hub_connect(const char* port)
+{
+    if(ahp_hub_is_connected())
+        return 0;
+    if(!ahp_serial_OpenComport(port)) {
+        if(!ahp_serial_SetupPort(9600, "8N1", 0)) {
+            ahp_hub_connected = 1;
+            return 0;
+        }
+        ahp_serial_CloseComport();
+    }
+    return 1;
+}
+
+void ahp_hub_disconnect()
+{
+    if(ahp_hub_is_connected()) {
+        ahp_serial_CloseComport();
+        if(mutexes_initialized) {
+            pthread_mutex_unlock(&mutex);
+            pthread_mutex_destroy(&mutex);
+            pthread_mutexattr_destroy(&mutex_attr);
+            mutexes_initialized = 0;
+        }
+        ahp_hub_connected = 0;
+    }
+}
+
+unsigned int ahp_hub_is_connected()
+{
+    return ahp_hub_connected;
+}
+
+void ahp_hub_send_command(unsigned char* buf, size_t len)
+{
+    ahp_serial_SendBuf(buf, len);
+}
---
+++ ahp_hub.h.cmake
@@ -0,0 +1,151 @@
+/**
+* \license
+*    MIT License
+*
+*    libahp_hub library to drive the AHP XC HUB control port
+*    Copyright (C) 2021  Ilia Platone
+*
+*    Permission is hereby granted, free of charge, to any person obtaining a copy
+*    of this software and associated documentation files (the "Software"), to deal
+*    in the Software without restriction, including without limitation the rights
+*    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*    copies of the Software, and to permit persons to whom the Software is
+*    furnished to do so, subject to the following conditions:
+*
+*    The above copyright notice and this permission notice shall be included in all
+*    copies or substantial portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*    SOFTWARE.
+*/
+
+#ifndef _AHP_HUB_H
+#define _AHP_HUB_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+#ifdef _WIN32
+#include <windows.h>
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT extern
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+
+/**
+* \mainpage AHP® HUB Controllers driver library API Documentation
+* \section Introduction
+*
+* The AHP HUB is a special device used for smart communication with the XC cross-correlators<br>
+* <br>
+* This software is meant to work with the HUB series motor controllers
+* visit https://www.iliaplatone.com/xc-hub for more informations and purchase options.
+*
+* \author Ilia Platone
+* \version @AHPHUB_VERSION@
+* \date 2017-2021
+* \copyright MIT License.
+*/
+
+/**
+ * \defgroup HUB_API AHP® HUB Controllers API
+ *
+ * This library contains functions for direct low-level usage of the AHP motor controllers.<br>
+ *
+ * This documentation describes utility, applicative and hardware control functions included into the library.<br>
+ * Each section and component is documented for general usage.
+*
+* \{
+* \defgroup Defs Types
+* \{*/
+
+/**\}
+ * \defgroup Defines Defines
+ *\{*/
+///AHP_HUB_VERSION This library version
+#define AHP_HUB_VERSION @AHP_HUB_VERSION@
+
+/**\}
+ * \defgroup Conn Connection
+ * \{*/
+
+ /**
+ * \brief Obtain the current libahp-hub version
+ * \return The current API version
+ */
+ DLL_EXPORT inline unsigned int ahp_hub_get_version(void) { return AHP_HUB_VERSION; }
+
+/**
+* \brief Connect to the HUB controller
+* \param port The serial port filename
+* \return non-zero on failure
+*/
+DLL_EXPORT int ahp_hub_connect(const char* port);
+
+/**
+* \brief Connect to the HUB controller using an existing file descriptor
+* \param fd The serial stream file descriptor
+* \return non-zero on failure
+*/
+DLL_EXPORT int ahp_hub_connect_fd(int fd);
+
+/**
+* \brief Return the file descriptor of the port connected to the HUB controllers
+* \param fd The serial stream file descriptor
+* \return The serial stream file descriptor
+*/
+DLL_EXPORT int ahp_hub_get_fd();
+
+/**
+* \brief Disconnect from the HUB controller
+*/
+DLL_EXPORT void ahp_hub_disconnect();
+
+/**
+* \brief Get the file descriptor that links to the controller
+* \return The file descriptor
+* \sa ahp_hub_connect
+*/
+DLL_EXPORT int ahp_hub_get_fd();
+
+/**
+* \brief Report connection status
+* \return non-zero if already connected
+* \sa ahp_hub_connect
+* \sa ahp_hub_connect_fd
+* \sa ahp_hub_disconnect
+*/
+DLL_EXPORT unsigned int ahp_hub_is_connected();
+
+/**\}
+ * \defgroup SG Communication
+ * \{*/
+
+ /**
+ * \brief Send a command to the control port
+ * \param buf the command string or buffer
+ * \param len the length of the command in bytes
+ * \sa ahp_hub_connect
+ * \sa ahp_hub_connect_fd
+ * \sa ahp_hub_disconnect
+ */
+ DLL_EXPORT void ahp_hub_send_command(unsigned char* buf, size_t len);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+/**\}
+ * \}*/
+#endif //_AHP_HUB_H
